a08705c8fb8e2928d2bad088319ae708c6cf057c
Vincent Nadoll <vincent.nadoll@googlemail.com>
2022-04-29 12:11:01 +0200

    Initilaize empty Spring Boot project

---
+++ src/main/java/de/kammerchorwernigerode/telegram/TelegramBotApplication.java
@@ -0,0 +1,17 @@
+package de.kammerchorwernigerode.telegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * {@link SpringBootApplication Spring Boot application}-class that is the entrypoint into this application.
+ *
+ * @author Vincent Nadoll
+ */
+@SpringBootApplication
+public class TelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotApplication.class, args);
+    }
+}
---
+++ src/test/java/de/kammerchorwernigerode/telegram/TelegramBotApplicationTest.java
@@ -0,0 +1,15 @@
+package de.kammerchorwernigerode.telegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Vincent Nadoll
+ */
+@SpringBootTest
+class TelegramBotApplicationTest {
+
+    @Test
+    void startingApplicationContext_shouldSucceed() {
+    }
+}
