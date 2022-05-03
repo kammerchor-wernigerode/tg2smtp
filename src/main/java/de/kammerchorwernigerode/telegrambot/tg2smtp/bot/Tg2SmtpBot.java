565936eaf637fc135925ecb83cf124b16810a238
Vincent Nadoll <vincent.nadoll@googlemail.com>
2022-05-03 18:39:28 +0200

    Remove unused import

--- src/main/java/de/kammerchorwernigerode/telegrambot/tg2smtp/bot/Tg2SmtpBot.java
+++ src/main/java/de/kammerchorwernigerode/telegrambot/tg2smtp/bot/Tg2SmtpBot.java
@@ -9,7 +9,6 @@ import org.springframework.lang.Nullable;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
