39341843c9f9604fca42ca5a1d37be1bea640e76
Vincent Nadoll <vincent.nadoll@googlemail.com>
2022-05-20 14:13:02 +0200

    Add specialized text message printer

--- src/main/java/de/kammerchorwernigerode/telegrambot/tg2smtp/bot/Tg2SmtpBotConfiguration.java
+++ src/main/java/de/kammerchorwernigerode/telegrambot/tg2smtp/bot/Tg2SmtpBotConfiguration.java
@@ -1,11 +1,16 @@
 package de.kammerchorwernigerode.telegrambot.tg2smtp.bot;
 
+import de.kammerchorwernigerode.telegrambot.tg2smtp.format.app.PrinterService;
+import de.kammerchorwernigerode.telegrambot.tg2smtp.format.model.Printer;
+import de.kammerchorwernigerode.telegrambot.tg2smtp.format.model.PrinterRegistry;
 import de.kammerchorwernigerode.telegrambot.tg2smtp.longpolling.FilteringLongPollingBot;
+import de.kammerchorwernigerode.telegrambot.tg2smtp.support.Configurer;
 import org.springframework.boot.autoconfigure.mail.MailProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.telegram.telegrambots.meta.api.objects.polls.Poll;
 import org.telegram.telegrambots.meta.generics.LongPollingBot;
 
 /**
@@ -13,11 +18,18 @@ import org.telegram.telegrambots.meta.generics.LongPollingBot;
  */
 @Configuration(proxyBeanMethods = false)
 @EnableConfigurationProperties({Tg2SmtpBotProperties.class})
-class Tg2SmtpBotConfiguration {
+class Tg2SmtpBotConfiguration implements Configurer {
 
     @Bean
-    public LongPollingBot tg2SmtpBot(Tg2SmtpBotProperties properties, JavaMailSender mailSender, MailProperties mailProperties) {
-        Tg2SmtpBot tg2SmtpBot = new Tg2SmtpBot(properties, mailSender, mailProperties);
+    public LongPollingBot tg2SmtpBot(Tg2SmtpBotProperties properties, JavaMailSender mailSender, MailProperties mailProperties,
+                                     PrinterService printerService) {
+        Tg2SmtpBot tg2SmtpBot = new Tg2SmtpBot(properties, mailSender, mailProperties, printerService);
         return new FilteringLongPollingBot(tg2SmtpBot, new ChatFilter(properties.getChatId()), tg2SmtpBot);
+    }
+
+    @Override
+    public void addPrinters(PrinterRegistry registry) {
+        registry.addPrinter(Poll.class, Printer.nullSafe(new PollPrinter()));
+        registry.addPrinter(String.class, new TextPrinter());
     }
 }
