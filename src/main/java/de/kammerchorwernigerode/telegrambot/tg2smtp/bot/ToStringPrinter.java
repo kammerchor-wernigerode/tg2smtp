c832b1833abaa45341007b68765e864ec66ca82b
Vincent Nadoll <vincent.nadoll@googlemail.com>
2022-05-20 22:56:23 +0200

    Import annotation

--- src/main/java/de/kammerchorwernigerode/telegrambot/tg2smtp/bot/ToStringPrinter.java
+++ src/main/java/de/kammerchorwernigerode/telegrambot/tg2smtp/bot/ToStringPrinter.java
@@ -1,6 +1,7 @@
 package de.kammerchorwernigerode.telegrambot.tg2smtp.bot;
 
 import de.kammerchorwernigerode.telegrambot.tg2smtp.format.model.Printer;
+import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 
 import java.util.Objects;
@@ -12,7 +13,7 @@ import java.util.Objects;
  */
 public class ToStringPrinter implements Printer<Object> {
 
-    @org.springframework.lang.NonNull
+    @NonNull
     @Override
     public String print(@Nullable Object object) {
         return Objects.toString(object, "");
