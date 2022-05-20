f9775634eea17f184333120eff9b1bcdcdd26ece
Vincent Nadoll <vincent.nadoll@googlemail.com>
2022-05-20 22:56:59 +0200

    Allow null argument

--- src/main/java/de/kammerchorwernigerode/telegrambot/tg2smtp/bot/ToStringPrinter.java
+++ src/main/java/de/kammerchorwernigerode/telegrambot/tg2smtp/bot/ToStringPrinter.java
@@ -4,6 +4,7 @@ import de.kammerchorwernigerode.telegrambot.tg2smtp.format.model.Printer;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 
+import java.util.Locale;
 import java.util.Objects;
 
 /**
@@ -17,5 +18,11 @@ public class ToStringPrinter implements Printer<Object> {
     @Override
     public String print(@Nullable Object object) {
         return Objects.toString(object, "");
+    }
+
+    @NonNull
+    @Override
+    public String print(@Nullable Object object, @Nullable Locale locale) {
+        return print(object);
     }
 }
