c28267a99e0bd9d18d0162bd689319e9452f23d3
Vincent Nadoll <vincent.nadoll@googlemail.com>
2022-05-26 19:49:58 +0200

    Run integration tests in CI only

--- src/test/java/de/kammerchorwernigerode/telegrambot/tg2smtp/Tg2SmtpApplicationTests.java
+++ src/test/java/de/kammerchorwernigerode/telegrambot/tg2smtp/Tg2SmtpApplicationTests.java
@@ -1,8 +1,10 @@
 package de.kammerchorwernigerode.telegrambot.tg2smtp;
 
+import de.kammerchorwernigerode.telegrambot.tg2smtp.test.IntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@IntegrationTest
 @SpringBootTest({"telegrambots.enabled=false"})
 class Tg2SmtpApplicationTests {
 
