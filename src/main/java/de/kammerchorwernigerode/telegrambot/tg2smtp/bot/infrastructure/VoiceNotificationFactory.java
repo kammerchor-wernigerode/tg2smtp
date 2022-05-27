a01129468928e780ceb542909b5feadd786d88db
Vincent Nadoll <vincent.nadoll@googlemail.com>
2022-05-27 21:36:20 +0200

    Refactor voice message notification creation

--- src/main/java/de/kammerchorwernigerode/telegrambot/tg2smtp/bot/infrastructure/VoiceNotificationFactory.java
+++ src/main/java/de/kammerchorwernigerode/telegrambot/tg2smtp/bot/infrastructure/VoiceNotificationFactory.java
@@ -3,21 +3,21 @@ package de.kammerchorwernigerode.telegrambot.tg2smtp.bot.infrastructure;
 import de.kammerchorwernigerode.telegrambot.tg2smtp.bot.model.Downloader;
 import de.kammerchorwernigerode.telegrambot.tg2smtp.notification.MetadataHeadedNotificationDecorator;
 import de.kammerchorwernigerode.telegrambot.tg2smtp.notification.Notification;
-import de.kammerchorwernigerode.telegrambot.tg2smtp.notification.app.FreemarkerNotification;
-import de.kammerchorwernigerode.telegrambot.tg2smtp.notification.app.TemplateBuilder;
 import de.kammerchorwernigerode.telegrambot.tg2smtp.notification.model.NotificationFactory;
+import de.kammerchorwernigerode.telegrambot.tg2smtp.notification.model.Renderer;
 import de.kammerchorwernigerode.telegrambot.tg2smtp.telegram.model.Metadata;
-import freemarker.template.Configuration;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Voice;
 
+import java.io.IOException;
+import java.util.Locale;
+
 /**
- * {@link NotificationFactory} that creates templated {@link FreemarkerNotification}s from Telegram {@link Voice}
- * messages.
+ * {@link NotificationFactory} that creates templated
+ * {@link MetadataHeadedNotificationDecorator metadata subject decorated} {@link VoiceNotification}s from Telegram
+ * {@link Voice} messages.
  *
  * @author Vincent Nadoll
  */
@@ -25,21 +25,34 @@ import org.telegram.telegrambots.meta.api.objects.Voice;
 @RequiredArgsConstructor
 public class VoiceNotificationFactory implements NotificationFactory<Voice> {
 
-    private final @NonNull Configuration configuration;
     private final @NonNull Downloader<MediaReference> downloader;
 
     @Override
     public Notification create(@NonNull Voice voice, @NonNull Metadata metadata) {
-        TemplateBuilder template = new TemplateBuilder("voice.ftl").locale(metadata.getLocale());
-
-        Notification notification = new FreemarkerNotification(template, configuration)
-                .with(download(voice));
+        VoiceNotification notification = new VoiceNotification(voice, downloader, metadata.getLocale());
         return new MetadataHeadedNotificationDecorator(metadata, notification);
     }
 
-    @SneakyThrows
-    private Resource download(Voice voice) {
-        MediaReference mediaReference = new MediaReference(voice.getFileId());
-        return downloader.download(mediaReference);
+
+    private static final class VoiceNotification extends MediaNotification {
+
+        private final Voice voice;
+        private final Locale locale;
+
+        public VoiceNotification(Voice voice, Downloader<MediaReference> downloader, Locale locale) {
+            super(downloader);
+            this.voice = voice;
+            this.locale = locale;
+        }
+
+        @Override
+        protected MediaReference create() {
+            return new MediaReference(voice.getFileId());
+        }
+
+        @Override
+        public String getMessage(@NonNull Renderer renderer) throws IOException {
+            return renderer.render("voice.ftl", locale, new Object());
+        }
     }
 }
--- src/test/java/de/kammerchorwernigerode/telegrambot/tg2smtp/bot/infrastructure/VoiceNotificationFactoryTests.java
+++ src/test/java/de/kammerchorwernigerode/telegrambot/tg2smtp/bot/infrastructure/VoiceNotificationFactoryTests.java
@@ -1,22 +1,19 @@
 package de.kammerchorwernigerode.telegrambot.tg2smtp.bot.infrastructure;
 
 import de.kammerchorwernigerode.telegrambot.tg2smtp.bot.model.Downloader;
-import freemarker.template.Configuration;
-import lombok.SneakyThrows;
+import de.kammerchorwernigerode.telegrambot.tg2smtp.notification.MetadataHeadedNotificationDecorator;
+import de.kammerchorwernigerode.telegrambot.tg2smtp.notification.Notification;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.core.io.Resource;
 import org.telegram.telegrambots.meta.api.objects.Voice;
 
 import static de.kammerchorwernigerode.telegrambot.tg2smtp.bot.infrastructure.Metadatas.createDefault;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 /**
  * @author Vincent Nadoll
@@ -26,18 +23,16 @@ class VoiceNotificationFactoryTests {
 
     private VoiceNotificationFactory factory;
 
-    private @Mock Configuration configuration;
     private @Mock Downloader<MediaReference> downloader;
 
     @BeforeEach
     void setUp() {
-        factory = new VoiceNotificationFactory(configuration, downloader);
+        factory = new VoiceNotificationFactory(downloader);
     }
 
     @Test
     void initializingNullArguments_shouldThrowException() {
-        assertThrows(IllegalArgumentException.class, () -> new VoiceNotificationFactory(null, downloader));
-        assertThrows(IllegalArgumentException.class, () -> new VoiceNotificationFactory(configuration, null));
+        assertThrows(IllegalArgumentException.class, () -> new VoiceNotificationFactory(null));
     }
 
     @Test
@@ -53,16 +48,11 @@ class VoiceNotificationFactoryTests {
     }
 
     @Test
-    @SneakyThrows
-    void creatingFromVoice_shouldDelegateDownload() {
+    void creatingNotification_shouldDecorate() {
         Voice voiceMessage = mock(Voice.class);
-        Resource attachment = mock(Resource.class);
-        MediaReference mediaReference = new MediaReference("foo");
-        when(voiceMessage.getFileId()).thenReturn("foo");
-        when(downloader.download(eq(mediaReference))).thenReturn(attachment);
 
-        factory.create(voiceMessage, createDefault());
+        Notification notification = factory.create(voiceMessage, createDefault());
 
-        verify(downloader).download(mediaReference);
+        assertTrue(notification instanceof MetadataHeadedNotificationDecorator);
     }
 }
