e0d6ecf5137f33b333b4248472962b380dd374f0
Vincent Nadoll <vincent.nadoll@googlemail.com>
2022-05-27 19:59:17 +0200

    Refactor photo notification creation

--- src/main/java/de/kammerchorwernigerode/telegrambot/tg2smtp/bot/infrastructure/PhotoNotificationFactory.java
+++ src/main/java/de/kammerchorwernigerode/telegrambot/tg2smtp/bot/infrastructure/PhotoNotificationFactory.java
@@ -4,22 +4,24 @@ import de.kammerchorwernigerode.telegrambot.tg2smtp.bot.app.PhotoPicker;
 import de.kammerchorwernigerode.telegrambot.tg2smtp.bot.model.Downloader;
 import de.kammerchorwernigerode.telegrambot.tg2smtp.notification.MetadataHeadedNotificationDecorator;
 import de.kammerchorwernigerode.telegrambot.tg2smtp.notification.Notification;
-import de.kammerchorwernigerode.telegrambot.tg2smtp.notification.app.FreemarkerNotification;
-import de.kammerchorwernigerode.telegrambot.tg2smtp.notification.app.TemplateBuilder;
 import de.kammerchorwernigerode.telegrambot.tg2smtp.notification.model.NotificationFactory;
+import de.kammerchorwernigerode.telegrambot.tg2smtp.notification.model.Renderer;
 import de.kammerchorwernigerode.telegrambot.tg2smtp.telegram.model.Metadata;
 import de.kammerchorwernigerode.telegrambot.tg2smtp.telegram.model.TitledPhotos;
-import freemarker.template.Configuration;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
 /**
- * {@link NotificationFactory} that creates templated {@link FreemarkerNotification}s from Telegram {@link PhotoSize}
- * messages.
+ * {@link NotificationFactory} that creates templated
+ * {@link MetadataHeadedNotificationDecorator metadata subject decorated} {@link PhotoNotification}s from Telegram
+ * {@link PhotoSize} messages.
  *
  * @author Vincent Nadoll
  */
@@ -27,24 +29,44 @@ import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 @RequiredArgsConstructor
 public class PhotoNotificationFactory implements NotificationFactory<TitledPhotos> {
 
-    private final @NonNull Configuration configuration;
     private final @NonNull PhotoPicker photoPicker;
     private final @NonNull Downloader<MediaReference> downloader;
 
     @Override
     public Notification create(@NonNull TitledPhotos photos, @NonNull Metadata metadata) {
-        TemplateBuilder template = new TemplateBuilder("photo.ftl").locale(metadata.getLocale());
-        PhotoSize photo = photoPicker.pickFrom(photos.getContent());
-
-        Notification notification = new FreemarkerNotification(template, configuration)
-                .with(download(photo))
-                .with("model", photos.getCaption().orElse(null));
+        Notification notification = new PhotoNotification(photos, photoPicker, downloader, metadata.getLocale());
         return new MetadataHeadedNotificationDecorator(metadata, notification);
     }
 
-    @SneakyThrows
-    private Resource download(PhotoSize photo) {
-        MediaReference mediaReference = new MediaReference(photo.getFileId());
-        return downloader.download(mediaReference);
+
+    private static final class PhotoNotification extends MediaNotification {
+
+        private final TitledPhotos photos;
+        private final PhotoPicker photoPicker;
+        private final Locale locale;
+
+        public PhotoNotification(TitledPhotos photos,
+                                 PhotoPicker photoPicker,
+                                 Downloader<MediaReference> downloader,
+                                 Locale locale) {
+            super(downloader);
+            this.photos = photos;
+            this.photoPicker = photoPicker;
+            this.locale = locale;
+        }
+
+        @Override
+        protected MediaReference create() {
+            PhotoSize photo = photoPicker.pickFrom(photos.getContent());
+            return new MediaReference(photo.getFileId());
+        }
+
+        @Override
+        public String getMessage(@NonNull Renderer renderer) throws IOException {
+            Map<String, Object> model = new HashMap<>();
+            model.put("model", photos.getCaption().orElse(null));
+
+            return renderer.render("photo.ftl", locale, model);
+        }
     }
 }
