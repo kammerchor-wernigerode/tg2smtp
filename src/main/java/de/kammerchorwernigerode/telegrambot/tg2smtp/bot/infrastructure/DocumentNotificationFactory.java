0c14d1557da066e0b80d3e8b99c83b57473df457
Vincent Nadoll <vincent.nadoll@googlemail.com>
2022-05-27 19:15:02 +0200

    Refactor document notification creation

--- src/main/java/de/kammerchorwernigerode/telegrambot/tg2smtp/bot/infrastructure/DocumentNotificationFactory.java
+++ src/main/java/de/kammerchorwernigerode/telegrambot/tg2smtp/bot/infrastructure/DocumentNotificationFactory.java
@@ -3,12 +3,10 @@ package de.kammerchorwernigerode.telegrambot.tg2smtp.bot.infrastructure;
 import de.kammerchorwernigerode.telegrambot.tg2smtp.bot.model.Downloader;
 import de.kammerchorwernigerode.telegrambot.tg2smtp.notification.MetadataHeadedNotificationDecorator;
 import de.kammerchorwernigerode.telegrambot.tg2smtp.notification.Notification;
-import de.kammerchorwernigerode.telegrambot.tg2smtp.notification.app.FreemarkerNotification;
-import de.kammerchorwernigerode.telegrambot.tg2smtp.notification.app.TemplateBuilder;
 import de.kammerchorwernigerode.telegrambot.tg2smtp.notification.model.NotificationFactory;
+import de.kammerchorwernigerode.telegrambot.tg2smtp.notification.model.Renderer;
 import de.kammerchorwernigerode.telegrambot.tg2smtp.telegram.model.Metadata;
 import de.kammerchorwernigerode.telegrambot.tg2smtp.telegram.model.TitledDocument;
-import freemarker.template.Configuration;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
@@ -16,9 +14,16 @@ import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Document;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Stream;
+
 /**
- * {@link NotificationFactory} that creates templated {@link FreemarkerNotification}s from Telegram {@link Document}
- * messages.
+ * {@link NotificationFactory} that creates templated
+ * {@link MetadataHeadedNotificationDecorator metadata subject decorated} {@link DocumentNotification}s from Telegram
+ * {@link Document} messages.
  *
  * @author Vincent Nadoll
  */
@@ -26,22 +31,38 @@ import org.telegram.telegrambots.meta.api.objects.Document;
 @RequiredArgsConstructor
 public class DocumentNotificationFactory implements NotificationFactory<TitledDocument> {
 
-    private final @NonNull Configuration configuration;
     private final @NonNull Downloader<MediaReference> downloader;
 
     @Override
     public Notification create(@NonNull TitledDocument document, @NonNull Metadata metadata) {
-        TemplateBuilder template = new TemplateBuilder("document.ftl").locale(metadata.getLocale());
-
-        FreemarkerNotification notification = new FreemarkerNotification(template, configuration)
-                .with(download(document.getContent()))
-                .with("model", document.getCaption().orElse(null));
+        DocumentNotification notification = new DocumentNotification(document, metadata.getLocale());
         return new MetadataHeadedNotificationDecorator(metadata, notification);
     }
 
-    @SneakyThrows
-    private Resource download(Document document) {
-        MediaReference mediaReference = new MediaReference(document.getFileId(), document.getFileName());
-        return downloader.download(mediaReference);
+
+    @RequiredArgsConstructor
+    private final class DocumentNotification implements Notification {
+
+        private final TitledDocument document;
+        private final Locale locale;
+
+        @Override
+        public String getMessage(@NonNull Renderer renderer) throws IOException {
+            Map<String, Object> model = new HashMap<>();
+            model.put("model", document.getCaption().orElse(null));
+
+            return renderer.render("document.ftl", locale, model);
+        }
+
+        @Override
+        public Stream<Resource> listAttachments() {
+            return Stream.of(download(document.getContent()));
+        }
+
+        @SneakyThrows
+        private Resource download(Document document) {
+            MediaReference mediaReference = new MediaReference(document.getFileId(), document.getFileName());
+            return downloader.download(mediaReference);
+        }
     }
 }
--- src/test/java/de/kammerchorwernigerode/telegrambot/tg2smtp/bot/infrastructure/DocumentNotificationFactoryTests.java
+++ src/test/java/de/kammerchorwernigerode/telegrambot/tg2smtp/bot/infrastructure/DocumentNotificationFactoryTests.java
@@ -1,23 +1,20 @@
 package de.kammerchorwernigerode.telegrambot.tg2smtp.bot.infrastructure;
 
 import de.kammerchorwernigerode.telegrambot.tg2smtp.bot.model.Downloader;
+import de.kammerchorwernigerode.telegrambot.tg2smtp.notification.MetadataHeadedNotificationDecorator;
+import de.kammerchorwernigerode.telegrambot.tg2smtp.notification.Notification;
 import de.kammerchorwernigerode.telegrambot.tg2smtp.telegram.model.TitledDocument;
-import freemarker.template.Configuration;
-import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.core.io.Resource;
 import org.telegram.telegrambots.meta.api.objects.Document;
 
 import static de.kammerchorwernigerode.telegrambot.tg2smtp.bot.infrastructure.Metadatas.createDefault;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 /**
  * @author Vincent Nadoll
@@ -27,18 +24,16 @@ class DocumentNotificationFactoryTests {
 
     private DocumentNotificationFactory factory;
 
-    private @Mock Configuration configuration;
     private @Mock Downloader<MediaReference> downloader;
 
     @BeforeEach
     void setUp() {
-        factory = new DocumentNotificationFactory(configuration, downloader);
+        factory = new DocumentNotificationFactory(downloader);
     }
 
     @Test
     void initializingNullArguments_shouldThrowException() {
-        assertThrows(IllegalArgumentException.class, () -> new DocumentNotificationFactory(null, downloader));
-        assertThrows(IllegalArgumentException.class, () -> new DocumentNotificationFactory(configuration, null));
+        assertThrows(IllegalArgumentException.class, () -> new DocumentNotificationFactory(null));
     }
 
     @Test
@@ -55,18 +50,12 @@ class DocumentNotificationFactoryTests {
     }
 
     @Test
-    @SneakyThrows
-    void creatingFromDocument_shouldDelegateDownload() {
+    void creatingNotification_shouldDecorate() {
         Document document = mock(Document.class);
         TitledDocument titledDocument = new TitledDocument("bar.pdf", document);
-        Resource attachment = mock(Resource.class);
-        MediaReference mediaReference = new MediaReference("foo", "bar.pdf");
-        when(document.getFileId()).thenReturn("foo");
-        when(document.getFileName()).thenReturn("bar.pdf");
-        when(downloader.download(eq(mediaReference))).thenReturn(attachment);
 
-        factory.create(titledDocument, createDefault());
+        Notification notification = factory.create(titledDocument, createDefault());
 
-        verify(downloader).download(mediaReference);
+        assertEquals(MetadataHeadedNotificationDecorator.class, notification.getClass());
     }
 }
