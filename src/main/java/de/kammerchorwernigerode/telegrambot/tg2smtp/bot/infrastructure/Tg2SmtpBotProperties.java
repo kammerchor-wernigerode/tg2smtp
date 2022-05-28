c233fa0c7625531d6289bc0495956ce607e1b0f4
Vincent Nadoll <vincent.nadoll@googlemail.com>
2022-05-28 21:11:22 +0200

    Remove unused import

--- src/main/java/de/kammerchorwernigerode/telegrambot/tg2smtp/bot/infrastructure/Tg2SmtpBotProperties.java
+++ src/main/java/de/kammerchorwernigerode/telegrambot/tg2smtp/bot/infrastructure/Tg2SmtpBotProperties.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.Collections;
 import java.util.Set;
 
 /**
